3d77976bf9966940734bded5a11b2e731894077c
mazharudaipuri <mazharudaipuri@gmail.com>
2025-02-13 11:57:25 +0000

    conversion of datatypes

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = 55
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"55" => 55 number will be the output
+// "55abc" => NaN
+// true => 1; flase => 0
+
+let isLoggedIn = "mazhar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "mazhar" => true
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
