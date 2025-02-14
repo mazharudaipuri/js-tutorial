6c809a8cb4fdc5a0350be67a3f8e179ad1e3f924
mazharudaipuri <mazharudaipuri@gmail.com>
2025-02-14 10:29:31 +0000

    Operation Code

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -26,3 +26,31 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
 
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = "mazhar"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
